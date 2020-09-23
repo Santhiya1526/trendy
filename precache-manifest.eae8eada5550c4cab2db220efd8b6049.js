082c7be1cc878649992a1f0bcad5045b179dd6aa
Santhiya1526 <santhiyamadhu1526@gmail.com>
2020-09-23 07:33:10 -0700

    Updates

---
+++ precache-manifest.eae8eada5550c4cab2db220efd8b6049.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b13dc2633444107c38768615d57b6c",
+    "url": "/trendy/index.html"
+  },
+  {
+    "revision": "aadc4dcd88defbfc87a1",
+    "url": "/trendy/static/css/2.cc2d33b2.chunk.css"
+  },
+  {
+    "revision": "3cf0fbcf9948fecb6229",
+    "url": "/trendy/static/css/main.5d461b17.chunk.css"
+  },
+  {
+    "revision": "aadc4dcd88defbfc87a1",
+    "url": "/trendy/static/js/2.3422991c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trendy/static/js/2.3422991c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf0fbcf9948fecb6229",
+    "url": "/trendy/static/js/main.9c94fe83.chunk.js"
+  },
+  {
+    "revision": "0b0bfdf5a9331d035063",
+    "url": "/trendy/static/js/runtime-main.ac88882c.js"
+  },
+  {
+    "revision": "4e7f73c33f599a54a81e896ab2c6146e",
+    "url": "/trendy/static/media/Logo1.4e7f73c3.jpg"
+  },
+  {
+    "revision": "55f9a0f8d3e62887bcfe3eef77a2267d",
+    "url": "/trendy/static/media/Logo2.55f9a0f8.jpg"
+  },
+  {
+    "revision": "5cc93eef1357329b6890209a95af7b16",
+    "url": "/trendy/static/media/Logo3.5cc93eef.jpg"
+  },
+  {
+    "revision": "fed45a2b2f70c3188f466b99c9ad7afc",
+    "url": "/trendy/static/media/Logo4.fed45a2b.jpg"
+  },
+  {
+    "revision": "65039d77dcbe177208e8470277222f60",
+    "url": "/trendy/static/media/Logo5.65039d77.jpg"
+  },
+  {
+    "revision": "4991249013e3ab53f08913f2f3189b9e",
+    "url": "/trendy/static/media/Logo6.49912490.jpg"
+  },
+  {
+    "revision": "4456009131864c5b25f9343e801fb85d",
+    "url": "/trendy/static/media/Logo7.44560091.jpg"
+  },
+  {
+    "revision": "fb28e40cff482467e16bdd3ef898342c",
+    "url": "/trendy/static/media/Logo8.fb28e40c.jpg"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/trendy/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cb2dc0471c688732a04b5f41672577e9",
+    "url": "/trendy/static/media/cottons.cb2dc047.jpg"
+  },
+  {
+    "revision": "97e1b24b25b30487b2b404139cdcb778",
+    "url": "/trendy/static/media/s.97e1b24b.jpg"
+  },
+  {
+    "revision": "180aac2466cc53c8e0506bbef832e734",
+    "url": "/trendy/static/media/softs.180aac24.jpg"
+  },
+  {
+    "revision": "4174b21e2e7755a2489f134ea5542005",
+    "url": "/trendy/static/media/w4.4174b21e.jpg"
+  },
+  {
+    "revision": "d9fcc7e5de235ee4ede0c2d00f989529",
+    "url": "/trendy/static/media/w41.d9fcc7e5.jpg"
+  },
+  {
+    "revision": "432f2bddeee8fdc253503db376d71183",
+    "url": "/trendy/static/media/w6.432f2bdd.jpg"
+  }
+]);
